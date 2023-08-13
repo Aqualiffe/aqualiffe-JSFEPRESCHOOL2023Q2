9ba515994536ee2dbe59c35d688d974c994b4067
Aqualiffe <evgeniia.er@gmail.com>
2023-08-13 18:47:54 +0300

    init: add config

--- library/index.js
+++ library/index.js
@@ -36,3 +36,21 @@ document.addEventListener("DOMContentLoaded", function() {
         console.log('клик внe блока');
     })
 })
+
+
+
+console.log(`
+Всего: 50 \n
+1. Вёрстка соответствует макету. Ширина экрана 768px +26.
+\t блок <header> +2
+\t секция Welcome +2
+\t секция About +4.
+\t секция Favorites +2
+\t Сделать кнопку own для последней книги +2
+\t секция CoffeShop +4
+\t секция Contacts +4
+\t секция LibraryCard +4
+\t блок <footer> + 2 \n
+2. Нет полосы прокрутки, весь контент сохраняется +12 \n
+3. На ширине экрана 768рх реализовано адаптивное меню +12
+`)
