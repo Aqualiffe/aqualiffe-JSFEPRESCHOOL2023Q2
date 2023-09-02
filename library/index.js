18b4adb369f0268c42cfa40e2a5f65d8fca08a33
Aqualiffe <evgeniia.er@gmail.com>
2023-09-02 04:20:29 +0300

    feat: modal form

--- library/index.js
+++ library/index.js
@@ -14,10 +14,19 @@ console.log(`
 3. На ширине экрана 768рх реализовано адаптивное меню +12
 `)
 
+const burger = document.getElementById("burger");
+const main_header = document.querySelector(".main-header");
+const menuItems = document.querySelectorAll('.menu-element');
+const dropMenu = document.querySelector(".dropMenu");
+const logo = document.querySelector(".logo");
+const buttonLogIn = document.querySelector('.log-in');
+const modal = document.querySelector('.modal');
+const modalLogIn = document.querySelector('.modal_logIn');
+
 /* Бургер меню */
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("burger").addEventListener("click", function() {
-        document.querySelector(".main-header").classList.toggle("open")
+    burger.addEventListener("click", function() {
+        main_header.classList.toggle("open")
     })
 })
 
@@ -30,16 +39,15 @@ document.addEventListener("DOMContentLoaded", function() {
 })
 
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("burger").addEventListener("click", function(e) {
+    burger.addEventListener("click", function(e) {
         e.isClick = true;   
         // console.log('клик внутри2');
     })
 })
-const menuItems = document.querySelectorAll('.menu-element');
 
 menuItems.forEach(e => {
     e.addEventListener("click", function() {
-        document.querySelector(".main-header").classList.toggle("open")
+        main_header.classList.toggle("open")
         // console.log("клик по ссылке")
     })
 })
@@ -47,14 +55,17 @@ menuItems.forEach(e => {
 document.addEventListener("DOMContentLoaded", function() {
     document.body.addEventListener("click", function(e) {
         if (e.isClick == true) return;
-        document.querySelector(".main-header").classList.remove("open");        
+        main_header.classList.remove("open");        
         // console.log('клик внe блока');
     })
 })
 
 /* Profile menu*/
-const dropMenu = document.querySelector(".dropMenu");
-const logo = document.querySelector(".logo");
+
+const removeDropMenuOpen = function() {
+         dropMenu.classList.remove("open");
+};
+
 
 document.addEventListener("DOMContentLoaded", function() {
     logo.addEventListener("click", function() {
@@ -63,10 +74,50 @@ document.addEventListener("DOMContentLoaded", function() {
     })
 })
 
+
 document.addEventListener('click', function(e) {
     const clickMenu=e.composedPath().includes(dropMenu);
     const clickLogo=e.composedPath().includes(logo);
     if (!clickMenu & !clickLogo) {
         dropMenu.classList.remove("open");
     }
-})
+});
+
+
+// modal
+
+
+
+// modal.style.cssText = `
+//     displey: flex;
+//     visibility: hidden;
+//     opacity: 0;
+//     transition: opacity 300ms easy-in-out;
+// `;
+ 
+// const closeModal = function(e) {
+//     const target = e.target;
+//     if (target === modal) {
+//        modal.classList.add('close');
+//     }
+// };
+
+
+const openModal = function() {
+    modal.classList.add('open');
+    // modal.style.visibility = 'visible';
+    // modal.style.opacity = 1;
+};
+
+buttonLogIn.addEventListener('click', function(){
+    openModal();
+    removeDropMenuOpen();
+});
+
+document.addEventListener('click', function(e) {
+    const clickModal=e.composedPath().includes(modal);
+    console.log(clickModal);
+    if (!clickModal) {
+        modal.classList.remove('open');
+    }
+});
