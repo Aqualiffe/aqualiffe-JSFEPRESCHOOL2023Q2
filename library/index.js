ae3e2b59a7b6819054e5237f4314889565df9a7f
Aqualiffe <evgeniia.er@gmail.com>
2023-09-08 15:51:06 +0300

    feat: slider

--- library/index.js
+++ library/index.js
@@ -1,18 +1,18 @@
-console.log(`
-Всего: 50 \n
-1. Вёрстка соответствует макету. Ширина экрана 768px +26.
-\t блок <header> +2
-\t секция Welcome +2
-\t секция About +4.
-\t секция Favorites +2
-\t Сделать кнопку own для последней книги +2
-\t секция CoffeShop +4
-\t секция Contacts +4
-\t секция LibraryCard +4
-\t блок <footer> + 2 \n
-2. Нет полосы прокрутки, весь контент сохраняется +12 \n
-3. На ширине экрана 768рх реализовано адаптивное меню +12
-`)
+// console.log(`
+// Всего: 50 \n
+// 1. Вёрстка соответствует макету. Ширина экрана 768px +26.
+// \t блок <header> +2
+// \t секция Welcome +2
+// \t секция About +4.
+// \t секция Favorites +2
+// \t Сделать кнопку own для последней книги +2
+// \t секция CoffeShop +4
+// \t секция Contacts +4
+// \t секция LibraryCard +4
+// \t блок <footer> + 2 \n
+// 2. Нет полосы прокрутки, весь контент сохраняется +12 \n
+// 3. На ширине экрана 768рх реализовано адаптивное меню +12
+// `)
 
 
 /* Бургер меню */
@@ -161,5 +161,63 @@ function Copy(containerid) {
 }
 
 
-// Слайдер
+// Покупка книг
+
+// смена имени кнопки
+
+
+//слайдер
+
+const btnLeft = document.querySelector('.carret_left');
+const btnRight = document.querySelector('.carret_right');
+const slider = document.querySelector('.gallery-imgs');
+const slides = document.querySelectorAll('.img_about-gallery');
+const dots = document.querySelectorAll('.dot');
+const slideCount = slides.length;
+const slideOffset = 0;
+const dotCount = dots.length;
+let slideIndex = 0;
+let dotIndex = 0;
+
+const activeDot = (e) => {
+    for(dot of dots) {
+        dot.classList.remove('active');        
+    }
+    dots[e].classList.add('active');
+};
+
+
+const slide = () => {
+    const imageWidth = 475;
+    const slideOffset = -slideIndex * imageWidth;
+    slider.style.transform = `translateX(${slideOffset}px)`;
+    activeDot(slideIndex);
+    if (slideOffset == 0 ) {
+        btnLeft.setAttribute('disabled', true)
+    }else if (slideOffset != 0 & slideOffset != -1900 ) {
+        btnLeft.removeAttribute('disabled');
+        btnRight.removeAttribute('disabled');
+    } else if (slideOffset == -1900) {
+        btnRight.setAttribute('disabled', true);        
+    }
+};
+
+
+dots.forEach((item, indexDot)=> {
+    item.addEventListener('click', () => {
+        slideIndex = indexDot;
+        slide();
+    })
+})
+
+btnLeft.addEventListener('click', () => {    
+    slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+    slide();   
+});
+  
+btnRight.addEventListener('click', () => {    
+    slideIndex = (slideIndex + 1) % slideCount;
+    slide();    
+});
+
 
