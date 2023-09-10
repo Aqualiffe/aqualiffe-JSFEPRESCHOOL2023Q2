f57d8819a94acb69254c99956fb0457050c80335
Aqualiffe <evgeniia.er@gmail.com>
2023-09-10 23:21:03 +0300

    feat: add cooments

--- library/index.js
+++ library/index.js
@@ -1,18 +1,13 @@
-// console.log(`
-// Всего: 50 \n
-// 1. Вёрстка соответствует макету. Ширина экрана 768px +26.
-// \t блок <header> +2
-// \t секция Welcome +2
-// \t секция About +4.
-// \t секция Favorites +2
-// \t Сделать кнопку own для последней книги +2
-// \t секция CoffeShop +4
-// \t секция Contacts +4
-// \t секция LibraryCard +4
-// \t блок <footer> + 2 \n
-// 2. Нет полосы прокрутки, весь контент сохраняется +12 \n
-// 3. На ширине экрана 768рх реализовано адаптивное меню +12
-// `)
+console.log(`
+Есть модальные окна! \n
+Регистрации нет! \n
+Бургер и другие кнопки до регистрации работают, кроме кнопок Buy \n
+Оба слайдера работают \n
+
+
+С подсчетом баллов запуталась =(
+`)
+
 
 
 /* Бургер меню */
@@ -162,33 +157,35 @@ function Copy(containerid) {
 
 
 // Покупка книг
-const btnsBook = document.querySelectorAll('.book_button');
-const modalBuyCard = document.querySelector('.modal-buy-a-card');
-const btnBuyClose = document.querySelector('.x-buy-a-card');
-const actBuyCard = document.querySelector('.modal_buy-a-card');
 
 
-const openModalBuyCard = function() {
-    modalBuyCard.classList.add("open");
-};
+// const btnsBook = document.querySelectorAll('.book_button');
+// const modalBuyCard = document.querySelector('.modalIn');
+// const btnBuyClose = document.querySelector('.x-log');
+// const actBuyCard = document.querySelector('.x-log');
 
-btnsBook.forEach(e => {
-    e.addEventListener('click', () => {
-        openModalBuyCard();        
-    })    
-});
 
-const closeModalBuyCard = function(e) {
-    const clickBuyClose = e.composedPath().includes(btnBuyClose);
-    // const clickBuyCard = e.composedPath().includes(actBuyCard);    
-    // const clickBtnBooks = e.composedPath().includes(btnsBook);
-    if (clickBuyClose){      
-        modalBuyCard.classList.remove('open');
-    }
+// const openModalBuyCard = function() {
+//     modalBuyCard.classList.add("open");
+// };
+
+// btnsBook.forEach(e => {
+//     e.addEventListener('click', () => {
+//         openModalBuyCard(); 
+//     })    
+// });
+
+// const closeModalBuyCard = function(e) {
+//     const clickBuyClose = e.composedPath().includes(btnBuyClose);
+//     const clickBuyCard = e.composedPath().includes(actBuyCard);    
+//     const clickBtnBooks = e.composedPath().includes(btnsBook);
+//     if (clickBuyClose){      
+//         modalBuyCard.classList.remove('open');
+//     }
     
-};
+// };
 
-document.addEventListener('click', closeModalBuyCard);
+// document.addEventListener('click', closeModalBuyCard);
 
 // смена имени кнопки
 
