ea8acb9e682f9d057e8e11a5decc5c1b859f633c
Aqualiffe <evgeniia.er@gmail.com>
2023-09-24 16:47:29 +0300

    feat: add btn play, next and prev audio. Add change audio and text

---
+++ js30-audio-player/index.js
@@ -0,0 +1,78 @@
+let audio = document.querySelector(".music"),
+    btnImg = document.querySelector(".play-btn"),
+    audioName = document.querySelector(".music-name"),
+    audioImg = document.querySelector(".music-img"),
+    audioTitle = document.querySelector(".title"),
+    progressBar = document.querySelector(".time"),
+    progressContainer = document.querySelector(".bar");
+
+
+let isPlay = false;
+
+function play() {
+    btnImg.src = "./assets/svg/pause.png";
+    isPlay = true;
+    audio.play();
+}
+
+function pause() {
+    btnImg.src ="./assets/svg/play.png";
+    isPlay = false;
+    audio.pause();
+}
+
+document.querySelector('.btn').addEventListener('click', () => {    
+    if(!isPlay) {
+        play();
+    } else {
+        pause();
+    }
+})
+
+let audioIndex = 0;
+
+const songs = [
+    {
+        name: "Don't Hurt Yourself",
+        path: "./assets/audio/beyonce.mp3",
+        img: "./assets/img/lemonade.png",
+        singer: "Beyonce"
+    },
+    {
+        name: "Don't Start Now",
+        path: "./assets/audio/dontstartnow.mp3",
+        img: "./assets/img/dontstartnow.png",
+        singer: "Dua Lipa"
+    },
+    {
+        name: "Headbanger",
+        path: "./assets/audio/headbanger.mp3",
+        img: "./assets/img/headbanger.jpg",
+        singer: "Babymetal"
+    }  
+];
+
+function changeAudio(song) {
+    audioName.innerHTML = songs[audioIndex].name;
+    audio.src = songs[audioIndex].path;
+    audioImg.src = songs[audioIndex].img;
+    audioTitle.innerHTML = songs[audioIndex].singer;
+}
+
+document.querySelector('.next_btn').addEventListener('click', () => {
+    audioIndex ++;
+    if(audioIndex > songs.length - 1) {   
+        audioIndex = 0;   
+    } 
+    changeAudio(songs[audioIndex]);
+    play();    
+})
+
+document.querySelector('.prev_btn').addEventListener('click', () => {
+    audioIndex --;
+    if(audioIndex < 0) {   
+        audioIndex = songs.length - 1;   
+    } 
+    changeAudio(songs[audioIndex]);
+    play();    
+})
