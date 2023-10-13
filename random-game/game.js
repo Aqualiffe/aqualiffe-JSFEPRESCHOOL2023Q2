0e53f6e011a70abbbe739b9685f47b699bfb6eb7
Aqualiffe <evgeniia.er@gmail.com>
2023-10-13 22:15:32 +0300

    feat: game logic

--- random-game/game.js
+++ random-game/game.js
@@ -1,6 +1,9 @@
 const game = document.querySelector('.game');
 const countArray = [];
+let result = document.querySelector('.result');
 let count = 4;
+let firstOpen = null;
+let secondOpen = null;
 
 
 // function getNamder() {
@@ -29,7 +32,41 @@ for (const item of countArray) {
     let card = document.createElement('div');
     card.textContent = item;
     card.classList.add("cards")
+
+    card.addEventListener('click', () => {
+        if (card.classList.contains('open')|| card.classList.contains('final')){
+            return;
+        }
+        if (firstOpen !== null && secondOpen!==null) {
+            firstOpen.classList.remove('open')
+            secondOpen.classList.remove('open')
+            firstOpen = null;
+            secondOpen = null;
+        }
+
+        card.classList.add('open');
+        if (firstOpen === null) {
+            firstOpen = card;
+        } else {
+            secondOpen = card;
+        }
+        if (firstOpen !== null && secondOpen!==null) {
+          let firstOpenNum = firstOpen.textContent;
+          let secondOpenNum = secondOpen.textContent;
+
+          if (firstOpenNum === secondOpenNum) {
+            firstOpen.classList.add('final')
+            secondOpen.classList.add('final')
+          }
+
+        }
+        if (countArray.length === document.querySelectorAll('.final').length){
+            result.innerHTML = 'Победа!';
+        }
+
+    })
+
     game.append(card)
+
 }
 
-console.log(game);
